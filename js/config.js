89b828b0cc8fbbf8e267962afa280df7c672b265
Kent Kim <58772172+kentkim0326@users.noreply.github.com>
2026-07-01 12:11:09 +0900

    Add MAPBOX_TOKEN to ICOC_CONFIG

--- js/config.js
+++ js/config.js
@@ -4,6 +4,7 @@
    ============================================================ */
 
 window.ICOC_CONFIG = {
-  SUPABASE_URL:  'https://qxbxwggljdbaprkdufwh.supabase.co',
+   MAPBOX_TOKEN: 'pk.eyJ1IjoibWlsaXZlcnNlMTAwNCIsImEiOiJjbXF0NmltcjYwMWprMnpzaGFrdjF5YnA3In0.yqIA5e6OePYT65r-xZShLA',
+   SUPABASE_URL:  'https://qxbxwggljdbaprkdufwh.supabase.co',
   SUPABASE_ANON: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4Ynh3Z2dsamRiYXBya2R1ZndoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzODg1ODgsImV4cCI6MjA5Nzk2NDU4OH0.Labb_hliJi9VNkc0jeA0e567EIgg6_GbdVMrlywIco4',
 };
